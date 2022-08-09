63f0ac1cca90cf9506f7256c8618228bee440705
papa0four <papa.o.deuce@gmail.com>
2022-08-09 15:31:39 -0400

    backing up old dot files

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,10 +1,6 @@
-vim.cmd [[
-try
-  colorscheme darkplus
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=dark
-endtry
+local colorscheme = "darkplus"
 
-highlight Normal guibg=none
-]]
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  return
+end
